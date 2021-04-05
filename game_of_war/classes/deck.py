d43c7fd50c087d0e5ec09c3c31795f5436134446
Ismael Fuentes <32370252+IsmaFuentes@users.noreply.github.com>
2021-04-05 10:55:51 +0200

    Add files via upload

---
+++ game_of_war/classes/deck.py
@@ -0,0 +1,19 @@
+import random
+from classes.utils import suits, ranks
+from classes.card import Card
+
+class Deck:
+
+    def __init__(self):
+        self.all_cards = []
+        for suit in suits:
+            for rank in ranks:
+                # create card object
+                created_card = Card(suit, rank)
+                self.all_cards.append(created_card)
+    
+    def shuffle(self):
+        random.shuffle(self.all_cards)
+
+    def deal_one(self): 
+        return self.all_cards.pop()
